9e6bad701bd641b10e3f87dddba4d897bba8d603
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-10-23 17:01:51 -0400

    Update

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,61 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] board;
     public Board(Character[][] matrix) {
+        board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        if(checkforWin('X')){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        if(checkforWin('O')){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if(!checkforWin('O') && !checkforWin('X')){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfO()){
+            return "O";
+        }
+        else if(isInFavorOfX()){
+            return "X";
+        }
+        return "";
     }
 
+    public Boolean checkforWin(Character isXorO){
+        for(int i = 0; i < 3 ; i++){
+            if(board[i][0] == isXorO && board[i][1] == isXorO && board[i][2] == isXorO){
+                return true;
+            }
+        }
+        for(int i = 0; i < 3 ; i++){
+            if(board[0][i] == isXorO && board[1][i] == isXorO && board[2][i] == isXorO) {
+                return true;
+            }
+        }
+        if(board[0][0] == isXorO && board[1][1] == isXorO && board[2][2] == isXorO) {
+            return true;
+        }
+        if(board[2][0] == isXorO && board[1][1] == isXorO && board[0][2] == isXorO) {
+            return true;
+        }
+        return false;
+    }
 }
